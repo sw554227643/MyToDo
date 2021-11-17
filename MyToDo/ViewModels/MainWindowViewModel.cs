7055a816d48b42a62a4779805e8ff164510f693f
visioner <554227643@qq.com>
2021-11-18 00:48:25 +0800

    添加项目文件。

---
+++ MyToDo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MyToDo.Common.Models;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace MyToDo.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        public MainWindowViewModel()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
+            CreatMenuBar();
+        }
+
+        private ObservableCollection<MenuBar> menuBars;
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return menuBars; }
+            set { SetProperty(ref menuBars, value); }
+        }
+
+
+        private void CreatMenuBar()
+        {
+            MenuBars.Add(new MenuBar() { Icon = "Home", Title = "首页", NameSpace = "IndexView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSpace = "ToDoView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookPlus", Title = "备忘录", NameSpace = "MemoView" });
+            MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSpace = "SettingView" });
+        }
+    }
+}
---
+++ MyToDo/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace MyToDo.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Maximized) this.WindowState = WindowState.Normal;
+                else this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += (s, e) => { this.Close(); };
+
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+
+            ColorZone.MouseDoubleClick += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Normal) this.WindowState = WindowState.Maximized;
+                else this.WindowState = WindowState.Normal;
+            };
+        }
+
+        
+    }
+}
