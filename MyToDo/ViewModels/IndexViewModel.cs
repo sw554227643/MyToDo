56da57f2d30fac45cab82132cbf4f0bea0ed9e09
visioner <554227643@qq.com>
2021-11-23 22:13:47 +0800

    commit

---
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyToDo.ViewModels
+{
+   public class IndexViewModel:BindableBase
+    {
+    }
+}
