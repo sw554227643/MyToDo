e7577156abe46dc504d629fbb7e130f5a7a939a1
visioner <554227643@qq.com>
2021-12-02 00:41:56 +0800

    使用行为来实现load 方法

--- MyToDo/Common/Behaviors/LoadInstanceBehavior.cs
+++ MyToDo/Common/Behaviors/LoadInstanceBehavior.cs
@@ -21,22 +21,25 @@ namespace MyToDo.Common.Behivors
 
         // Using a DependencyProperty as the backing store for LoadInstance.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty LoadInstanceProperty =
-            DependencyProperty.RegisterAttached("LoadInstance", typeof(string), typeof(LoadInstanceBehavior), new PropertyMetadata(null,OnLoadInstanceChanged));
+            DependencyProperty.RegisterAttached("LoadInstance", typeof(string), typeof(LoadInstanceBehavior), new PropertyMetadata(null, OnLoadInstanceChanged));
 
         private static void OnLoadInstanceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
+            //获取到触发这个事件的元素
             FrameworkElement element = d as FrameworkElement;
             if (element != null)
             {
                 element.Loaded += (s, e2) =>
                 {
+                    //获取元素的DataContext
                     var viewmodel = element.DataContext;
                     if (viewmodel == null) return;
+                    //利用反射来触发方法
                     var methodInfo = viewmodel.GetType().GetMethod(e.NewValue.ToString());
                     if (methodInfo != null) methodInfo.Invoke(viewmodel, null);
                 };
             }
-            
+
         }
     }
 }
